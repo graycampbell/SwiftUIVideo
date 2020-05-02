d87a364358b539f65fb387539c923563e4402bf9
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2020-05-02 15:56:44 -0500

    Add project

---
+++ Sources/SwiftUIVideo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIVideo
+//
+//  Created by Gray Campbell on 5/2/20.
+//  Copyright © 2020 Gray Campbell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
